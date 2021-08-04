678b9c89f64650b0e40a1cd68cf4f248a0af6d47
AaronD07 <38016960+AaronD07@users.noreply.github.com>
2021-08-04 16:59:09 -0700

    list
    lambdas
    streams
    files

--- src/_03_To_Do_List/ToDoList.java
+++ src/_03_To_Do_List/ToDoList.java
@@ -1,6 +1,91 @@
 package _03_To_Do_List;
 
-public class ToDoList {
+import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+import java.util.ArrayList;
+import java.util.Random;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+
+
+
+public class ToDoList implements ActionListener{
+	private JLabel label = new JLabel();
+	private JFrame frme = new JFrame();
+	private JPanel panel = new JPanel();
+	private JButton button1 = new JButton("add task");
+	private JButton button2 = new JButton("view tasks");
+	private JButton button3 = new JButton("remove task");
+	private JButton button4 = new JButton("save list");
+	private JButton button5 = new JButton("load list");
+	ArrayList<String> buttonsList = new ArrayList<String>();
+
+	public ToDoList() {
+		frme.setVisible(true);
+		frme.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frme.setLayout(new FlowLayout());
+		label.setName("to do list");
+		frme.add(label);
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		panel.add(button4);
+		panel.add(button5);
+		frme.add(panel);
+
+		button1.addActionListener((event) -> {
+String oneQ = JOptionPane.showInputDialog("Enter a task");
+buttonsList.add(oneQ);
+		});
+		button2.addActionListener((event)-> {
+			for(int i=0; i<buttonsList.size(); i++) {
+				
+			
+			System.out.print(buttonsList.get(i) + ", ");
+			}
+		});
+		button3.addActionListener((event)-> {
+			System.out.println( );
+			String remove = JOptionPane.showInputDialog("Which # on list would you like to remove?");
+			int remove2 = Integer.parseInt(remove);
+			buttonsList.remove(remove2-1);
+			for(int i=0; i<buttonsList.size(); i++) {
+				
+				
+				System.out.print(buttonsList.get(i) + ", ");
+				}
+		});
+		button4.addActionListener((event)->{
+			  
+			    String two = JOptionPane.showInputDialog("Enter a title for your text");
+
+			    try( PrintStream out = new PrintStream( new File( two ) ) ) {
+			        out.println( buttonsList );
+			    } catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+		});
+	}
+	public static void main(String[] args) {
+		new ToDoList();
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+
+	}
+	
 	/*
 	 * Create a program with five buttons, add task, view tasks, remove task, save list, and load list. 
 	 *
